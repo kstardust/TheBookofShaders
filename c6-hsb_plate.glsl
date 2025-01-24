3e38cac8256e414639962e6fbf6e1f877160a326
stardust <lql9705@gmail.com>
2025-01-24 10:59:45 +0900

    hsb

---
+++ c6-hsb_plate.glsl
@@ -0,0 +1,31 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+#define PI 3.1415926
+
+uniform vec2 u_resolution;
+uniform float u_time;
+
+//  Function from Iñigo Quiles
+//  https://www.shadertoy.com/view/MsS3Wc
+vec3 hsb2rgb( in vec3 c ){
+    vec3 rgb = clamp(abs(mod(c.x*6.0+vec3(0.0,4.0,2.0),
+                             6.0)-3.0)-1.0,
+                     0.0,
+                     1.0 );
+    rgb = rgb*rgb*(3.0-2.0*rgb);
+    return c.z * mix( vec3(1.0), rgb, c.y);
+}
+
+void main() {
+    vec2 st = gl_FragCoord.xy / u_resolution;
+    vec2 ct = 0.5 - st;
+
+    // convert to polar coordination
+    float a = atan(ct.y, ct.x) / (2.0*PI) + 0.5;
+    float r = 2.0*length(ct.xy);
+
+    vec3 color = hsb2rgb(vec3(a, r, 1.0));
+    gl_FragColor = vec4(color, 1.0);
+}
